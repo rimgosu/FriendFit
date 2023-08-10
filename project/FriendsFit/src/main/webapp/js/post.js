65a64453d816716feae1800cf537186cac1a3985
rimgosu <newnyup@gmail.com>
2023-08-10 11:02:33 +0900

    merged

---
+++ project/FriendsFit/src/main/webapp/js/post.js
@@ -0,0 +1,47 @@
+   const btn = document.querySelector("#btn");
+   
+   btn.addEventListener("click", () => { 
+      new daum.Postcode({
+         oncomplete: function(data) {
+         
+         var addr ='';
+         var extraAddr = '';
+         
+         if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                    addr = data.roadAddress;
+                } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                    addr = data.jibunAddress;
+                }   
+                
+                if(data.userSelectedType === 'R'){
+                    // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                    // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                    if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                        extraAddr += data.bname;
+                    }
+                    // 건물명이 있고, 공동주택일 경우 추가한다.
+                    if(data.buildingName !== '' && data.apartment === 'Y'){
+                        extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                    }
+                    // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                    if(extraAddr !== ''){
+                        extraAddr = ' (' + extraAddr + ')';
+                    }
+                    // 조합된 참고항목을 해당 필드에 넣는다.
+                    document.getElementById("addr3").value = extraAddr;
+                
+                } else {
+                    document.getElementById("addr3").value = '';
+                }
+                
+                document.getElementById('postcode').value = data.zonecode;
+                document.getElementById("addr1").value = addr;
+                // 커서를 상세주소 필드로 이동한다.
+                document.getElementById("addr2").focus();
+                    
+         }
+      }).open();
+   });
+                           
+               
+   
